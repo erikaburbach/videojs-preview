732e870d1f40a903c0d245835cf36deef5b07bce
eburbach <eburbach@brightcove.com>
2017-02-10 21:37:25 +0000

    add default styles not present in preview player js
    default style including height/width aren't set in preview players. add
    these styles so players appear when height/width are absent.

--- extension/content.js
+++ extension/content.js
@@ -8,3 +8,15 @@ if(elements.length>0){
     currentDiv=currentDiv.nextSibling;
   }
 }
+
+var players=document.getElementsByClassName("video-js");
+if (players.length>0){
+  for (i=0;i<players.length;i++){
+    if(!players[i].style.height){
+    players[i].style.height="150px";
+    players[i].style.width="300px";
+    players[i].style.fontSize="14px";
+    players[i].style.overflow="hidden";
+  }
+  }
+}
